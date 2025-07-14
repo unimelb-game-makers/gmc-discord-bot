70db976ff5bb0488ed65fe5547d697eafaa2fd67
jeffrey-22 <sjyjeffrey@qq.com>
2025-07-14 07:59:25 +0000

    Added setup guide

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gmc-discord-bot
 Custom python discord bot for UMGMC discord
+
+## Setup
+
+- Clone repository on UNIX machine
+- Install python and pip
+- Setup .env environment file
+- I use python venv, so python -m venv ~/py3venv (change to your venv directory)
+- ~/py3venv/bin/pip install -r requirements.txt
+- ~/py3venv/bin/python3 -m bot.main (To run the bot for testing)
+- Create system service for continuous deployment
--- bot/main.py
+++ bot/main.py
@@ -7,16 +7,30 @@ from discord.ext import commands
 # Load token from .env file
 load_dotenv()
 discord_bot_token = os.getenv("DISCORD_BOT_TOKEN")
+discord_guild_id = os.getenv("DISCORD_GUILD_ID")
 
-# Configure bot
+# Configure bot and guild
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='>', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# Bot ready
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+# Setup resync command.
+# Resync bot in case of command changes
+@bot.command(name='sync', description='Sync commands. Careful not to spam due to rate limit.')
+async def sync(ctx):
+    await bot.tree.sync()
+    await ctx.send('Command tree synced.')
 
 # Setup a testing ping command
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@bot.tree.command(name='ping', description="Test command.")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message('pong')
 
 # Start bot
-bot.run(discord_bot_token)
+if __name__ == "__main__":
+    bot.run(discord_bot_token)
